e50925656505343e5f7bb9141127eddd91c2018c
frenchboy89 <ice455210@gmail.com>
2022-12-08 10:47:37 +0100

    Added templates folder on project

--- blog/views.py
+++ blog/views.py
@@ -12,11 +12,14 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("Hello,world. You're at the blog about")   
+    return HttpResponse("Hello,world. You're at the blog about")  
+
 def pricing(request):
     return HttpResponse("Hello,world. You're at the blog pricing")
+
 def contact(request):
-    return HttpResponse("Hello,world. You're at the blog contact us")     
+    return HttpResponse("Hello,world. You're at the blog contact us")
+         
 def dashboard(request):
     return HttpResponse("Hello,world. You're at the blog dashboard")             
 
